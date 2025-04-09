96241ce8fc40ebeb3a69a3eef04ab54b68c8049b
Taka1234567891 <xiaochilde05@gmail.com>
2025-04-09 21:21:56 +0700

    up

--- src/app/(user)/materi/detail/[id]/page.tsx
+++ src/app/(user)/materi/detail/[id]/page.tsx
@@ -16,7 +16,6 @@ export default async function MateriDetailPage({ params }: MateriDetailPageProps
   if (error || !materi) {
     notFound();
   }
-
   return (
     <div className="container mx-auto px-4 py-12">
       <div className="max-w-4xl mx-auto">
